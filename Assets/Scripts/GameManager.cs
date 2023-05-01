c06ee270e30748a6a57d5d70611890092aa95a36
Roman Oktiabr <romanoktiabr@gmail.com>
2023-05-01 21:55:22 +0300

    Made the main part of the script
    Timebar, win\lose, restart game, etc

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,62 +1,62 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Slider _amountSlider;
-    [SerializeField] private TextMeshProUGUI _amountText;
-    [SerializeField] private RectTransform _gameField;
+
+
+    [SerializeField] private TextMeshProUGUI _barTimeText;
+    [SerializeField] private TextMeshProUGUI _setTimeText;
+    [SerializeField] private TextMeshProUGUI _setAmountText;
+    [SerializeField] private TextMeshProUGUI _startButtonText;
+    [SerializeField] private Slider _barTimeSlider;
+    [SerializeField] private Slider _setTimeSlider;
+    [SerializeField] private Slider _setAmountSlider;
     [SerializeField] private Numbers _buttonPrefab;
+    [SerializeField] private Button _startButton;
     [SerializeField] private GameObject _winSplashScreen;
     [SerializeField] private GameObject _loseSplashScreen;
+    [SerializeField] private RectTransform _gameField;
 
-    private GameState _gameState = GameState.Idle;
     private int _expectedButton = 1;
-    private int _amount;
+    private float _time;
+    private float _amount;
     private float _maxX;
     private float _maxY;
+    private bool _stopTimer;
     private const int MinSizeBorder = 40;
-    private List<Numbers> _numbers = new();
+
+    private void Start()
+    {
+        _stopTimer = true;
+
+    }
 
     public void AmountChange()
     {
-        _amount = (int)_amountSlider.value;
-        _amountText.text = "Amount of Numbers: " + _amount;
+        _amount = (int)_setAmountSlider.value;
+        _setAmountText.text = "Amount of numbers: " + _amount;
     }
 
-    private void Win()
+    public void GameTimeChange()
     {
-        Debug.Log("You Win!");
-    }
-
-    private void Lose()
-    {
-        foreach (Numbers num in _numbers)
-        {
-            Destroy(num.gameObject);
-        }
-        _numbers.Clear();
-        _loseSplashScreen.SetActive(true);
-    }
-
-    private void ButtonClicked(int number)
-    {
-        _numbers.Remove(_numbers[0]);
-        if (number != _expectedButton)
-        {
-            Lose();
-        }
-        ++_expectedButton;
-        if (_expectedButton == _amount + 1)
-        {
-            Win();
-        }
+        _time = _setTimeSlider.value;
+        _setTimeText.text = "Game time: " + _setTimeSlider.value.ToString() + " sec";
+        _barTimeText.text = "Set parameters below";
     }
 
     public void StartGame()
     {
+        _stopTimer = false;
+        _barTimeText.text = "Time left: " + _setTimeSlider.value;
+        _startButtonText.text = "Hurry up!";
+        _setTimeSlider.interactable = false;
+        _setAmountSlider.interactable = false;
+        _expectedButton = 1;
+        _winSplashScreen.SetActive(false);
+        _loseSplashScreen.SetActive(false);
+        _startButton.interactable = false;
         var counter = 1;
         while (counter <= _amount)
         {
@@ -68,8 +68,76 @@ public class GameManager : MonoBehaviour
             var spawnedButton = Instantiate(_buttonPrefab, _gameField);
             spawnedButton.transform.localPosition = new Vector2(randomX, randomY);
             spawnedButton.Initialize(counter, ButtonClicked);
-            _numbers.Add(spawnedButton);
             counter++;
+        }
+    }
+
+    private void ButtonClicked(int number)
+    {
+        if (number != _expectedButton)
+        {
+            Lose();
+        }
+        ++_expectedButton;
+        if (_expectedButton == _amount + 1)
+        {
+            Win();
+        }
+    }
+
+    public void DestroyAllNumbers()
+    {
+        GameObject[] numbers = GameObject.FindGameObjectsWithTag("Button");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Destroy(numbers[i]);
+        }
+    }
+
+    private void Win()
+    {
+        AmountChange();
+        GameTimeChange();
+        DestroyAllNumbers();
+        _stopTimer = true;
+        _winSplashScreen.SetActive(true);
+        _barTimeText.text = "Set parameters below";
+        _setTimeSlider.interactable = true;
+        _setAmountSlider.interactable = true;
+        _startButtonText.text = "Restart";
+        _startButton.interactable = true;
+    }
+
+    private void Lose()
+    {
+        AmountChange();
+        GameTimeChange();
+        DestroyAllNumbers();
+        _stopTimer = true;
+        _loseSplashScreen.SetActive(true);
+        _barTimeText.text = "Set parameters below";
+        _setTimeSlider.interactable = true;
+        _setAmountSlider.interactable = true;
+        _startButtonText.text = "Restart";
+        _startButton.interactable = true;
+    }
+    private void Update()
+    {
+
+        if (!_stopTimer)
+        {
+            _time -= Time.deltaTime;
+            float minutes = Mathf.FloorToInt(_time / 60);
+            float seconds = Mathf.FloorToInt(_time - minutes * 60);
+            string timeLeftText = string.Format("{0:0}:{1:00}", minutes, seconds);
+            _barTimeText.text = timeLeftText;
+            _barTimeSlider.value = _time;
+
+        }
+
+        if (_time < 0)
+        {
+            Lose();
         }
     }
 }
--- Assets/Scripts/GameState.cs
+++
@@ -1,7 +0,0 @@
-public enum GameState
-{
-    Idle,
-    Game,
-    Win,
-    Lose
-}
